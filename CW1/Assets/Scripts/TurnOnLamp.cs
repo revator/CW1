17fff6e958a1650d76d501da7a6a45d9daec1729
Unknown <sven.ulrich87@googlemail.com>
2017-11-30 13:11:20 +0000

    Imported package

---
+++ CW1/Assets/Scripts/TurnOnLamp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnLamp : MonoBehaviour
+{
+
+    // Variables
+    public GameObject lampLight;
+    public Light directionalLight;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        lampLight = GameObject.Find("LAt_Light");
+        lampLight.SetActive(false);
+        print("I OWORk");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Turnon();
+    }
+
+    void Turnon()
+    {
+        if (directionalLight.intensity <= 0.05f)
+        {
+            lampLight.SetActive(true);
+        }
+    }
+}
